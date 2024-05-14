876d4daa3058f62b9864ba36fa7bb02ae52ce182
RachelDuffin <rachel.g.duffin@gmail.com>
2024-05-14 13:21:07 +0100

    Final formatting and documentation

--- oncodeep_upload/__main__.py
+++ oncodeep_upload/__main__.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 """ __main__.py
 
-Entrypoint for qiagen_upload
+Entrypoint for oncodeep_upload
 """
 import os
+import subprocess
 import argparse
+from pathlib import Path
 from .oncodeep_upload import OncoDeepUpload
 from logger import Logger
 import config
-import toolbox
 
 
 def arg_parse() -> dict:
@@ -33,7 +34,7 @@ def arg_parse() -> dict:
     parser.add_argument(
         "-F",
         "--file_path",
-        type=lambda x: toolbox.is_valid_file(parser, x),
+        type=lambda x: is_valid_file(parser, x),
         help="Path of file to upload",
         required=True,
     )
@@ -61,18 +62,60 @@ def arg_parse() -> dict:
     return vars(parser.parse_args())
 
 
+def is_valid_file(parser: argparse.ArgumentParser, arg: str) -> str:
+    """
+    Check file path is valid
+        :param parser (argparse.ArgumentParser):    Holds necessary info to parse cmd
+                                                    line into Python data types
+        :param arg (str):                           Input argument
+        :return (str):                              Input argument
+    """
+    if not os.path.exists(arg):
+        parser.error(f"The file {arg} does not exist!")
+    else:
+        return arg  # Return argument
+
+
+def git_tag() -> str:
+    """
+    Obtain git tag from current commit
+        :return stdout (str):   String containing stdout,
+                                with newline characters removed
+    """
+    filepath = os.path.dirname(os.path.realpath(__file__))
+    cmd = f"git -C {filepath} describe --tags"
+
+    proc = subprocess.Popen(
+        [cmd], stderr=subprocess.PIPE, stdout=subprocess.PIPE, shell=True
+    )
+    out, _ = proc.communicate()
+    if out.decode("utf-8"):
+        return out.rstrip().decode("utf-8")
+    else:
+        return "[unversioned]"
+
+
 args = arg_parse()
 outdir = os.path.join(os.getcwd(), "outputs")
+filename = Path(args["file_path"]).stem
+
 logfile_path = os.path.join(
-    outdir, f"oncodeep_upload.{args['run_identifier']}.{config.TIMESTAMP}.log"
+    outdir, f"oncodeep_upload.{filename}.{config.TIMESTAMP}.log"
 )
 logger = Logger(__package__, logfile_path).logger
 
 if not os.path.isdir(outdir):
     os.mkdir(outdir)
 
-logger.info(f"Running oncodeep_upload {toolbox.git_tag()}")
+logger.info(f"Running oncodeep_upload {git_tag()}")
 
-okdm_upload = OncoDeepUpload(args['file_path'], args['run_identifier'], args['hostname'], args['username'], args['password'], logger)
+okdm_upload = OncoDeepUpload(
+    args["file_path"],
+    args["run_identifier"],
+    args["hostname"],
+    args["username"],
+    args["password"],
+    logger,
+)
 okdm_upload.upload_file()
 okdm_upload.close_connection()
--- oncodeep_upload/oncodeep_upload.py
+++ oncodeep_upload/oncodeep_upload.py
@@ -1,8 +1,10 @@
-"""oncodeep_upload.py
+#!/usr/bin/env python3
+""" oncodeep_upload.py
 
 This script uploads an input file to a specified destination folder on an SFTP server with specified
 hostname, username and password, using paramiko
 """
+
 import sys
 import os
 import paramiko
@@ -22,10 +24,10 @@ class OncoDeepUpload:
         hostname (str):                     SFTP server login hostname
         username (str):                     SFTP server login username
         password (str):                     SFTP server login password
-        sftp_client
-        (paramiko.sftp_client.SFTPClient):  SFTP client object for use in file upload
         ssh_client
         (paramiko.client.SSHClient):        SSH client object for use in file upload
+        sftp_client
+        (paramiko.sftp_client.SFTPClient):  SFTP client object for use in file upload
 
     Methods
         create_ssh_client()
@@ -51,16 +53,17 @@ class OncoDeepUpload:
         Constructor for the OncoDeepUpload class
             :param file_path (str):         File path of file to upload
             :param run_identifier (str):    Name of the run, to be used to create SFTP server subdir
-            :param subfolder (str):         Subfolder on SFTP server to load file into
-            :param logger (logging.Logger): Logger object
             :param hostname (str):          SFTP server hostname
             :param username (str):          SFTP server login username
             :param password (str):          SFTP server login password
+            :param logger (logging.Logger): Logger object
         """
         self.file_path = file_path
         self.run_identifier = run_identifier
         self.subfolder = f"/data/{self.run_identifier}/"
-        self.sftp_destination = os.path.join(self.subfolder, os.path.basename(file_path))
+        self.sftp_destination = os.path.join(
+            self.subfolder, os.path.basename(file_path)
+        )
         self.logger = logger
         # Remote server credentials
         self.hostname = hostname
@@ -113,11 +116,15 @@ class OncoDeepUpload:
             :return None:
         """
         try:
-            self.logger.info(f"Uploading file to SFTP server subfolder. Src: {self.file_path} Dest: {self.sftp_destination}")
+            self.logger.info(
+                f"Uploading file to SFTP server subfolder. Src: {self.file_path} Dest: {self.sftp_destination}"
+            )
             self.sftp_client.put(self.file_path, self.sftp_destination)
             self.logger.info("File upload successful")
         except Exception as exception:
-            self.logger.error(f"An exception was encountered when uploading the file to the SFTP server: {exception}")
+            self.logger.error(
+                f"An exception was encountered when uploading the file to the SFTP server: {exception}"
+            )
             sys.exit(1)
 
     def close_connection(self) -> None:
